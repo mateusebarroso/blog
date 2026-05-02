daf2f8e0589e11167edd9207c04121f145a304ca
MATEUS EVANGELISTA BARROSO <mateusebarroso2002@gmail.com>
2026-05-02 18:02:25 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+let menu = document.querySelector(".menu");
+let navLinks = document.querySelector(".nav-links");
+
+menu.addEventListener('click', e=>{
+   navLinks.classList.toggle("active")
+   
+})
